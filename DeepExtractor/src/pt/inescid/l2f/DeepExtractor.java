238d8710937718f845d5448a081956174cab8640
José Correia <josecorreia@Joses-MBP.lan>
2015-03-15 22:27:18 +0000

    ficheiro de build alterado, classe main alterada, e nova classe adicionada na package dependencyExtractor

--- DeepExtractor/src/pt/inescid/l2f/DeepExtractor.java
+++ DeepExtractor/src/pt/inescid/l2f/DeepExtractor.java
@@ -1,27 +1,36 @@
 package pt.inescid.l2f;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 
+import pt.inescid.l2f.dependencyExtractor.DependencyExtractor;
 import pt.inescid.l2f.xipapi.XipDocumentFactory;
 import pt.inescid.l2f.xipapi.domain.XipDocument;
+import pt.inescid.l2f.xipapi.domain.Dependency;
 
-public class DeepExplorer {
+
+public class DeepExtractor {
 	/**
 	 * Main function.
 	 *
 	 * @param args path/filename of the file that contains the XML output from XIP.
 	 */
 	public static void main(String[] args) {
-
+		DependencyExtractor de = new DependencyExtractor();
 		XipDocument document = null;
 		try{
+			System.out.println("Inicio");
 			XipDocumentFactory xipDocumentFactory = XipDocumentFactory.getInstance();
 			BufferedReader buffer = new BufferedReader(new FileReader(args[0]));
 			document = xipDocumentFactory.getXipResult(buffer);
-
+			
+			de.setDepedencyList(document.getDependencies());
+			
+			System.out.println("FIM");
+			
 		}catch(Exception e1){
-			System.err.println("AfterXIP: input error");
+			System.err.println("DeepExplorer: input error");
 			e1.printStackTrace();
 			return;
 		}
---
+++ DeepExtractor/src/pt/inescid/l2f/dependencyExtractor/DependencyExtractor.java
@@ -0,0 +1,20 @@
+package pt.inescid.l2f.dependencyExtractor;
+
+import pt.inescid.l2f.xipapi.domain.Dependency;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+
+public class DependencyExtractor {
+
+	private ArrayList<Dependency> dep1 = new ArrayList<Dependency>(); 
+	private HashSet<Dependency> dep2 = new HashSet<Dependency>(); 
+
+
+
+	public void setDepedencyList(Collection<Dependency> dep){
+		
+	}
+}
