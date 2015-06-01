77da0fb7c7c17889ebf33777da0ff483c34aa887
Jose Correia <zemiguel91@gmail.com>
2015-06-01 19:28:02 +0100

    apnes algumas formataç~es em alguns ficheiros

--- DeepExtractor/src/pt/inescid/l2f/dependencyExtractor/domain/dependency/Mod.java
+++ DeepExtractor/src/pt/inescid/l2f/dependencyExtractor/domain/dependency/Mod.java
@@ -1,65 +1,14 @@
 package pt.inescid.l2f.dependencyExtractor.domain.dependency;
 
-import java.util.ArrayList;
-
 import pt.inescid.l2f.dependencyExtractor.domain.database.Coocorrencia;
 import pt.inescid.l2f.dependencyExtractor.domain.database.Palavra;
 import pt.inescid.l2f.dependencyExtractor.domain.database.Propriedade;
-import pt.inescid.l2f.xipapi.domain.Dependency;
-import pt.inescid.l2f.xipapi.domain.Feature;
-import pt.inescid.l2f.xipapi.domain.Token;
-import pt.inescid.l2f.xipapi.domain.XIPNode;
+
 
 public class Mod extends DependencyType{
 
 	public Mod(Palavra palavra, Coocorrencia coo, Propriedade prop) {
 		super(palavra, coo, prop);
 	}
-
-	/*@Override
-	public void getDepedencyInformation(Dependency dep) {
-		long wordId1 = 0;
-		long wordId2 = 0;
-		String prop = "SEM_PROP";
-		String depname = dep.getName();
-		
-		for (Feature f : dep.getFeatures()){
-			//Propriedade
-			String aux = f.getName();
-			if (aux.equals("PRE")||aux.equals("POST")){
-				prop=aux;
-			}
-		}
-		_propriedade.checkProperty(prop, depname);
-			ArrayList<Long> a = new ArrayList<Long>();
-			//os dois nós da dependencia
-			int i = 0;
-			for (XIPNode node : dep.getNodes()){
-				String pos = node.getName();
-				String word = "";
-				for (Token token : node.getTokens()){
-					if(!word.isEmpty()){
-						word = word + " ";
-					}
-					word  = word + token.getLemmas().element();		
-					System.out.println(node.getNodeNumber() + " " + word);
-				i = token.getSentenceNumber();
-				}
-				a.add(super._palavra.checkWord(word, pos, "categoria"));
-			}
-			if (i == 3)
-				System.exit(0);
-			if(a.size()== 2){
-				super._coocorrencia.checkCoocorrence(a.get(0), a.get(1), prop, depname);
-			}
-			else{
-				//System.out.println("Depedencia com erro MOD");
-				//System.out.println(dep.printDependency());
-				//System.out.println(dep.getSentenceNumber());
-				//System.out.println("####");
-				
-			}
-		
-	}*/
 
 }
--- DeepExtractor/src/pt/inescid/l2f/dependencyExtractor/domain/dependency/Subj.java
+++ DeepExtractor/src/pt/inescid/l2f/dependencyExtractor/domain/dependency/Subj.java
@@ -1,14 +1,8 @@
 package pt.inescid.l2f.dependencyExtractor.domain.dependency;
 
-import java.sql.Connection;
-
 import pt.inescid.l2f.dependencyExtractor.domain.database.Coocorrencia;
 import pt.inescid.l2f.dependencyExtractor.domain.database.Palavra;
 import pt.inescid.l2f.dependencyExtractor.domain.database.Propriedade;
-import pt.inescid.l2f.xipapi.domain.Dependency;
-import pt.inescid.l2f.xipapi.domain.Feature;
-import pt.inescid.l2f.xipapi.domain.Token;
-import pt.inescid.l2f.xipapi.domain.XIPNode;
 
 public class Subj extends DependencyType{
 
@@ -16,50 +10,4 @@ public class Subj extends DependencyType{
 		super(palavra,coo, prop);
 	}
 
-	/*@Override
-	public void getDepedencyInformation(Dependency dep) {
-		long wordId1 = 0;
-		long wordId2 = 0;
-		String prop = "SEM_PROP";
-		String depname = dep.getName();
-		
-		for (Feature f : dep.getFeatures()){
-			//Propriedade
-			String aux = f.getName();
-			if (aux.equals("PRE")||aux.equals("POST")){
-				prop=aux;
-			}
-		}
-		_propriedade.checkProperty(prop, depname);
-			
-			//os dois nós da dependencia
-			int i = 1;
-			for (XIPNode node : dep.getNodes()){
-				String pos = node.getName();
-				String word = "";
-				for (Token token : node.getTokens()){
-					if(!word.isEmpty()){
-						word = word + " ";
-					}
-					word  = word + token.getLemmas().element();		
-				}
-				System.out.println(node.getNodeNumber() + " " + word);
-				long wordId = super._palavra.checkWord(word, pos, "categoria");
-				if(i==1){
-					wordId1 = wordId;
-				}else{
-					wordId2 = wordId;
-				}
-				i++;
-			}
-			
-			if(wordId1==0 || wordId2 == 0){
-				System.out.println("Depedencia com erro SUBJ");
-			}
-			
-			super._coocorrencia.checkCoocorrence(wordId1, wordId2, prop, depname);
-		
-		//System.out.println("##############");
-
-	}*/
 }
