4f351afe9d2b54f9a05c50bdefdaf4180a7f4e69
josepcorreia <zemiguel91@gmail.com>
2015-11-23 01:45:02 +0000

    como correr com o jar

--- DeepExtractor/runJAR.sh
+++ DeepExtractor/runJAR.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+#java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor path_to_db path_to_corpus
 java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor /Users/josecorreia/Projects/DB/ /Users/josecorreia/Projects/Parte1
+
+#java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures path_to_db
+java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures /Users/josecorreia/Projects/DB/
