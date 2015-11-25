9361bd403780c84cb2714dc122413cae202c4906
josepcorreia <zemiguel91@gmail.com>
2015-11-25 01:42:26 +0000

    script runJar actualizado read-me

--- Database/readme.txt
+++ Database/readme.txt
@@ -6,3 +6,5 @@ O nome da base de dados deve ser "db_deep.db".
 Para eliminar os dados presentes numa base de dados já existente, utiliza-se o script presente no ficheiro "cleanscript.sql". No terminal, abre-se a base de dados com o comando 'sqlite3 db_deep.db', e depois na consola do sqlite introduz-se '.read cleanscript.sql'.
 
 Após se concluir a extração das co-ocorrências, para criar índices nas tabelas de modo permitir pesquisas mais rápidas, utiliza-se o script presente no ficheiro "indexes_db.sql". No terminal, abre-se a base de dados com o comando 'sqlite3 db_deep.db', e depois na consola do sqlite introduz-se '.read indexes_db.sql'.
+
+Observação: durante a extração de co-ocorrências, o ficheiro da base de dados não deve estar num sistema afs, porque com o aumentar do seu tamanho, o tempo de resposta desta aumenta consideravelmente, fazendo com que a execução da ferramenta de extração também aumente.  
--- DeepExtractor/runJAR.sh
+++ DeepExtractor/runJAR.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
 #java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor path_to_db path_to_corpus
-java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor /Users/josecorreia/Projects/DB/ /Users/josecorreia/Projects/Parte1
+#java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures path_to_db
+
+
+#/ffs/tmp/string/CORPUS/
+corpus='/ffs/tmp/string/CORPUS'
+#corpus='/Users/josecorreia/Projects/CetemPublico'
+
+#/afs/l2f.inesc-id.pt/projects/tools/string/deepString/DB/
+db='/tmp/deepString_tmp/'
+#db='/Users/josecorreia/Projects/DB/'
+
+db_final='/afs/l2f.inesc-id.pt/projects/tools/string/deepString/DB_F/'
+#db_final='/afs/l2f.inesc-id.pt/projects/tools/string/deepString/DB_SF/'
+#db_final='/Users/josecorreia/Projects/DB/'
+
+
+
+mkdir logDeepExtractor
+cd DeepExplorer/DeepExtractor
+
+ant main > ../../logDeepExtractor/compile.txt
+
+for dir in $corpus/Parte*; do
+    echo "$dir"
+
+	base=$(basename "$dir")
+	fname=$base'.txt'
+  
+   	#java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor path_to_db path_to_corpus
+   	java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepExtractor $db $dir > ../../logDeepExtractor/$fname
+
+   	mkdir $db_final/$base
+   	cp $db/db_deep.db $db_final/$base
+done
 
 #java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures path_to_db
-java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures /Users/josecorreia/Projects/DB/
+java -cp jar/DeepExtractor.jar:lib/* pt.inescid.l2f.DeepMeasures $db > ../../logDeepExtractor/medidas.txt
+
+mkdir $db_final/medidas
+cp $db/db_deep.db $db_final/medidas
